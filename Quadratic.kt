08fc93018ba443e5b133d842b3cea0c7237d4eda
dqnid <dani.heras@hotmail.com>
2022-10-14 16:05:37 +0200

    Prompting number os solutions on quadratic equations

--- Quadratic.kt
+++ Quadratic.kt
@@ -1,11 +1,16 @@
 import kotlin.math.*
 fun main(){
-    val a:Double = 5.0
-    val b:Double = 1.0
-    val c:Double = 2.0
+    val a:Double = 1.0
+    val b:Double = 2.0
+    val c:Double = 1.0
 
-    val left = 0-a
-    val right = sqrt(b.pow(2) - (4*a*c))
-    val bot = 2*a
-   
+    val rightP = b.pow(2) - (4*a*c)
+    
+    if (rightP < 0){
+        println("0 solutions (has complex roots)")
+    } else if (sqrt(rightP) == 0.0){
+        println("1 solution")
+    } else {
+        println("2 solutions")
+    }
 }
